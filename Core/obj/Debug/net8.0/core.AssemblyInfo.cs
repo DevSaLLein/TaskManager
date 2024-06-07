15ed4242232b2ae129a4fa37880684acfe4e6b4a
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-06-07 11:01:21 -0300

    fix gitignore

--- Core/obj/Debug/net8.0/core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a415169a85ddbdf15ed0153313f75aecf1ef3413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86803a72f10ced211d9026fdd6dce4b2cee20afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
